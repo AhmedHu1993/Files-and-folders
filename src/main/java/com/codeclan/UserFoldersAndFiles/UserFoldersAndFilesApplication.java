7eeec8f8b0cc4660531bbb08b83db7e65ed97836
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-22 16:28:46 +0100

    initializng

---
+++ src/main/java/com/codeclan/UserFoldersAndFiles/UserFoldersAndFilesApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.UserFoldersAndFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFoldersAndFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFoldersAndFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/UserFoldersAndFiles/UserFoldersAndFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.UserFoldersAndFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFoldersAndFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
