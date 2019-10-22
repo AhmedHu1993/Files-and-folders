d98b207c2a6df238e4e30ef8be4349466e4044f2
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-22 17:07:20 +0100

    add methods

--- src/main/java/com/codeclan/UserFoldersAndFiles/components/DataLoader.java
+++ src/main/java/com/codeclan/UserFoldersAndFiles/components/DataLoader.java
@@ -1,5 +1,10 @@
 package com.codeclan.UserFoldersAndFiles.components;
 
+import com.codeclan.UserFoldersAndFiles.models.User;
+import com.codeclan.UserFoldersAndFiles.repositories.FileRepository;
+import com.codeclan.UserFoldersAndFiles.repositories.FolderRepository;
+import com.codeclan.UserFoldersAndFiles.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,7 +12,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    @Autowired
+    FileRepository fileRepository;
 
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     public void run(ApplicationArguments args) {
 
